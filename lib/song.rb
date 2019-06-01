b744a6d5d56367de584ee82cb293e2609daa28e6
Candice Parker <lexus2424@gmail.com>
2019-06-01 23:29:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,6 +37,10 @@ class Song
     #class finder
     self.all.find{|song| song.name == name}
   end
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
     
   
   
