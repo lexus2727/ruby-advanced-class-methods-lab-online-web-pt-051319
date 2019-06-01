5d73c3300cf8315458da8da1fef53f2eb78d1219
Candice Parker <lexus2424@gmail.com>
2019-06-01 23:21:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,13 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
+  def self.create
+    song = self.new #initializes a new song
+    @@all << song  #saves it to class variable
+    song  #returns the song instance initialized and saved
+  end
+  
   def self.all
     @@all
   end
@@ -9,5 +15,29 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.new_by_name(name)
+    #class constructor
+    #takes in string name of a song
+  song = self.new
+  song.name = name
+  song
+  end
+  
+  def self.create_by_name(name)
+    #class constructor
+    #takes in string name of a song
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+  
+  def self.find_by_name(name)
+    #class finder
+    self.all.find{|person| person.name == name}
+    
+  
+  
 
 end
